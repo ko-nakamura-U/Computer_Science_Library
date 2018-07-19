759a2e2da6131f523072e2a6f8c8b4cfdbee7a25
ko-nakamura-U <ko-nakamura@unext.jp>
2018-07-19 18:09:14 +0900

    Update CSL16

--- CSL16.ml
+++ CSL16.ml
@@ -16,3 +16,16 @@ type tree_t = Empty                          (* 空の木 *)
 type ekikan_tree_t =
     Empty
   | Node of string * (string * float) list * ekikan_tree_t * ekikan_tree_t;;
+
+(* 17.11 *)
+(* 目的：「駅名」と「駅名と距離の組みのリスト」を受け取ったら、その駅までの距離を返す *)
+(* assoc : string -> (string * float) list -> float *)
+let rec assoc_ ekimei lst = match lst with
+    [] -> infinity
+  | (e, k) :: rest ->
+      if e = ekimei then k else assoc_ ekimei rest;;
+
+(* 17.11 tests *)
+let test_17_11_1 = assoc_ "後楽園" [] = infinity
+let test_17_11_2 = assoc_ "後楽園" [("新大塚", 1.2); ("後楽園", 1.8)] = 1.8
+let test_17_11_3 = assoc_ "池袋" [("新大塚", 1.2); ("後楽園", 1.8)] = infinity;;
